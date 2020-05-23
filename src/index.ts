0fddeb9bdd826bfe191b787b3811c5592795ca37
jaybisaga <johnbisaga@gmail.com>
2020-05-23 18:22:25 -0500

    more fixes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import { MarkTemplate } from "./MarkTemplate";
-import { MarkVariable } from "./MarkVariable";
-
-export { MarkTemplate as MarkTemplate };
-export { MarkTemplate as MarkVariable };
+export { MarkTemplate } from "./MarkTemplate";
+export { MarkVariable } from "./MarkVariable";
 
