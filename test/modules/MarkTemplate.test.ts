e93ee6b90b2c3c194780ea8112691d90f3f34042
jaybisaga <johnbisaga@gmail.com>
2020-05-24 19:37:51 -0500

    new npm version

--- test/modules/MarkTemplate.test.ts
+++ test/modules/MarkTemplate.test.ts
@@ -95,12 +95,12 @@ describe ('MarkWahlberg', () => {
         });
 
         it('finds existing variable in text regardless of value', () => {
-            const str = "something something ${{ name: variable1, type: STRING, value: 'foo' }} something something";
+            const str = "Agreement for ${{name: variable1, type: STRING, defaultValue: 'defaultValue'}}";
             const vars = mark.getVariablesForInnerText(str);
             expect(vars).toHaveLength(1);
             expect(vars[0].variable.name).toBe(mark.getVariables()[0].variable.name);
-            expect(vars[0].strLength).toBe(50);
-            expect(vars[0].index).toBe(20);
+            expect(vars[0].strLength).toBe(64);
+            expect(vars[0].index).toBe(14);
         });
     });
 });
